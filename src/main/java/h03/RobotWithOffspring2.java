61bf01da97c1f7a8d86a6d15ae0167ec92561837
Ayham Al Wadi <93454946+Ayhal@users.noreply.github.com>
2022-11-18 05:32:12 +0100

    Update RobotWithOffspring2.java

--- src/main/java/h03/RobotWithOffspring2.java
+++ src/main/java/h03/RobotWithOffspring2.java
@@ -1,5 +1,17 @@
 package h03;
 
 public class RobotWithOffspring2 {
+    
+   public class RobotWithOffspring2 extends RobotWithOffspring{
+       private int directionAccu;
+       /* konstrukor **/
+       public RobotWithOffspring2(int numberOfColumnsOfWorld,int numberOfRowsOfWorld
+                                 ,Direction direction,int numberOfCoins){
+      super(numberOfColumnsOfWorld,numberOfRowsOfWorld, direction,numberOfCoins);
+      this.numberOfColumnsOfWorld=numberOfColumnsOfWorld;
+       this.numberOfRowsOfWorld=numberOfRowsOfWorld=;
+       }
+   
+   }
 
 }
